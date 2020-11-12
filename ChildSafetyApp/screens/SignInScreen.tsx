bd5cd63950883221a82403a850e32137e0ccc042
David Okao <davidokao@lawn-128-61-64-140.lawn.gatech.edu>
2020-11-12 17:26:52 -0500

    signing fix

--- ChildSafetyApp/screens/SignInScreen.tsx
+++ ChildSafetyApp/screens/SignInScreen.tsx
@@ -4,9 +4,8 @@ import * as React from 'react';
 import { RootStackParamList } from '../types';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet, TextInput, Button, Alert } from 'react-native';
+import { Alert, Button, Text, TouchableOpacity, TextInput, View, StyleSheet } from 'react-native';
 
-import { Text, View } from '../components/Themed';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
@@ -16,16 +15,32 @@ export default SignInScreen;
 
 
 function SignInScreen(){
+
+
   return (
-    <View>
-      <TextInput
-        placeholder="Username"
-      />
-      <TextInput
-        placeholder="Password"
-      />
-      <Button title="Sign in" onPress={()=> Alert.alert('Alert')} />
-    </View>
+<View style={styles.container}>
+      <Text style={styles.titleText}>Enter your Details</Text>
+        <TextInput
+          keyboardType = 'email-address'
+          placeholder='email'
+          placeholderTextColor = 'white'
+          style={styles.input}
+        />
+        <TextInput
+          placeholder={'password'}
+          secureTextEntry={true}
+          placeholderTextColor = 'white'
+          style={styles.input}
+        />
+        
+     
+        <TouchableOpacity
+          style={styles.button}
+       >
+         <Button title="Sign in" style={styles.buttonText}> Sign Up / Login </Button>
+       </TouchableOpacity>
+        
+      </View>
   );
 }
 
