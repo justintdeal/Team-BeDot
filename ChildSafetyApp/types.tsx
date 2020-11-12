de4413ccf5a3b3d602eb18ad5c5fecff683e75dd
Justin <jdeal1912@gmail.com>
2020-11-12 10:04:07 -0500

    three tabbed app

---
+++ ChildSafetyApp/types.tsx
@@ -0,0 +1,22 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  TabOne: undefined;
+  TabTwo: undefined;
+  TabThree: undefined;
+};
+
+export type TabOneParamList = {
+  TabOneScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
+
+export type TabThreeParamList = {
+  TabThreeScreen: undefined;
+};
