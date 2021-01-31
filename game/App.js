ada47e6bafc056ab492aefb80f0fc90c3785c35d
eebby3 <eebby3@gatech.edu>
2021-01-31 13:10:21 -0500

    created walls component and edited game logic in App.js

--- game/App.js
+++ game/App.js
@@ -1,16 +1,34 @@
-import React, { PureComponent } from "react";
+import React, { useState, PureComponent } from "react";
 import { AppRegistry, StyleSheet, Dimensions, View } from "react-native";
 import { GameLoop } from "react-native-game-engine";
+import Walls from './components/Walls'
  
-const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
 const RADIUS = 25;
+
+const screenWidth = Dimensions.get("screen").width
+const screenHeight = Dimensions.get("screen").height  // width and height of any screen
+//const birdLeft = screenWidth / 2   // very left corner of bird
+//const [birdBottom, setBirdBottom] = useState(screenHeight / 2) // [] is called a state and the useState allows for thing to be on middle of screen at all times
+const [wallsLeft, setWallsLeft] = useState(screenWidth)
+const [wallsLeftTwo, setWallsLeftTwo] = useState(screenWidth + screenWidth / 2 + 30)
+//const gravity = 3
+const [wallsNegHeight, setWallsNegHeight]  = useState(0)
+const [wallsNegHeightTwo, setWallsNegHeightTwo]  = useState(0)
+const [score, setScore] = useState(0)
+const wallWidth = 60
+const wallHeight = 300
+const gap = 200
+// let gameTimerID
+// let wallsLeftTimerId
+// let wallsLeftTimerIdTwo
+// const [isGameOver, setIsGameOver] = useState(false)
  
 export default class BestGameEver extends PureComponent {
   constructor() {
     super();
     this.state = {
-      x: WIDTH / 2 - RADIUS,
-      y: HEIGHT / 2 - RADIUS
+      x: screenWidth / 2 - RADIUS,
+      y: screenHeight / 2 - RADIUS
     };
   }
  
@@ -28,7 +46,27 @@ export default class BestGameEver extends PureComponent {
     return (
       <GameLoop style={styles.container} onUpdate={this.updateHandler}>
  
-        <View style={[styles.player, { left: this.state.x, top: this.state.y }]} />
+        <View style={[styles.player, { left: this.state.x, top: this.state.y }]}>
+
+        <Walls 
+        color = {'yellow'}
+        wallHeight = {wallHeight}
+        wallWidth = {wallWidth}
+        randomBottom = {wallsNegHeightTwo}
+        gap = {gap}
+        wallsLeft = {wallsLeftTwo}
+      />
+      <Walls 
+        color = {'yellow'}
+        wallHeight = {wallHeight}
+        wallWidth = {wallWidth}
+        randomBottom = {wallsNegHeightTwo}
+        gap = {gap}
+        wallsLeft = {wallsLeftTwo}
+      />
+        
+        
+      </View>
  
       </GameLoop>
     );
